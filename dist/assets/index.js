ffc654fb49da0a5202fdf42f53e43b92e25a12ea
LisaxLF <Lisafokkinga@gmail.com>
2024-04-23 10:58:04 +0200

    update build

--- dist/assets/index.js
+++ dist/assets/index.js
@@ -1,1 +1,14 @@
+console.log("Hello, world! this is js");
+const highlightedGames = document.querySelectorAll(".game-cover-wrapper");
+console.log(highlightedGames);
+highlightedGames.forEach((game) => {
+  game.addEventListener("click", async () => {
+    console.log("Game clicked:", game);
+    if (game.classList.contains("active")) {
+      game.classList.remove("active");
+    } else {
+      game.classList.add("active");
+    }
+  });
+});
 console.log("Hello, world!");
